8488d0fc4bb8ecb3719e16a32e94e03402940f81
Sam Duncliffe <sam.duncliffe@outlook.com>
2025-11-13 22:11:45 +0000

    FIguring out how Opentk works

---
+++ GraphicsWindow.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace _2D_Physics_Simulation_V2
+{
+    public class GraphicsWindow : GameWindow
+    {
+        float[] vertices = {
+            -0.5f, -0.5f, 0.0f, //Bottom-left vertex
+            0.5f, -0.5f, 0.0f, //Bottom-right vertex
+            0.0f,  0.5f, 0.0f  //Top vertex
+            };
+
+        int VertexBufferObject;
+
+        public GraphicsWindow(int width, int height, string title) : base(GameWindowSettings.Default, new NativeWindowSettings() { Size = (width, height), Title = title }) { }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+
+            if (KeyboardState.IsKeyDown(Keys.Escape))
+            {
+                Close();
+            }
+        }
+
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+
+            GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+            VertexBufferObject = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+            //Code goes here
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            //Code goes here.
+
+            SwapBuffers();
+        }
+
+        protected override void OnFramebufferResize(FramebufferResizeEventArgs e)
+        {
+            base.OnFramebufferResize(e);
+
+            GL.Viewport(0, 0, e.Width, e.Height);
+        }
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -7,7 +7,6 @@ namespace _2D_Physics_Simulation_V2
         static void Main(string[] args)
         {
             var sim = new Simulation(50);
-            var ani = new Animation(10, 1.5f, ref sim, 100, 100, System.Drawing.Color.Black, Vector.GetOrigin());
 
             sim.AddParticle(new Particle("1", 1, new Vector(-10, -10)));
             sim.AddParticle(new Particle("2", 1, new Vector(-10, 10)));
@@ -16,11 +15,10 @@ namespace _2D_Physics_Simulation_V2
             sim.GetParticle("1").ApplyImpulse(new Vector(10, 10));
             sim.GetParticle("2").ApplyImpulse(new Vector(10, -10));
 
-            ani.StartSimulationRecording();
-            ani.SaveFrames("Collision test");
-
-            Console.WriteLine("done");
-            Console.ReadLine();
+            using (var window = new GraphicsWindow(800, 600, "LearnOpenTK"))
+            {
+                window.Run();
+            }
         }
     }
 }
